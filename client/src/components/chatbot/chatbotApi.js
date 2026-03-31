03bd8c35cddf85a9de581e04378335f40880fd28
Manaswi Raj Sharma <manaswirajsharma1001@gmail.com>
2026-03-31 23:59:25 +0530

    calling fix

--- client/src/components/chatbot/chatbotApi.js
+++ client/src/components/chatbot/chatbotApi.js
@@ -4,7 +4,7 @@ import { BASE_URL } from "../../utils/constants.js"
 export const sendChatbotStep = async (action = null, value = null) => {
   try {
     const response = await axios.post(
-      {BASE_URL} + "/chatbot/chat", // IMPORTANT
+      BASE_URL + "/chatbot/chat",
       {
         action,
         value
