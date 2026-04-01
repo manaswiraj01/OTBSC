208a9a9d03897a422b973e71569d9a6cda491884
Manaswi Raj Sharma <manaswirajsharma1001@gmail.com>
2026-04-02 04:59:19 +0530

    fixed time format to ist

--- backend/src/utils/generateReceipt.js
+++ backend/src/utils/generateReceipt.js
@@ -40,6 +40,7 @@ export const generateReceipt = (booking, user) => {
 
             const formatDate = (date) =>
                 new Date(date).toLocaleDateString("en-IN", {
+                    timeZone: "Asia/Kolkata",
                     day: "2-digit",
                     month: "short",
                     year: "numeric",
@@ -47,11 +48,13 @@ export const generateReceipt = (booking, user) => {
 
             const formatDateTime = (date) =>
                 new Date(date).toLocaleString("en-IN", {
+                    timeZone: "Asia/Kolkata",
                     day: "2-digit",
                     month: "short",
                     year: "numeric",
                     hour: "2-digit",
                     minute: "2-digit",
+                    hour12: true,
                 });
 
             // =========================
