11677df72ca48af06fdce4956307b922a02cf158
khushibhati26 <147315718+khushibhati26@users.noreply.github.com>
2026-08-21 14:13:48 +0530

    Update email sender and recipient in sendOtpEmail

--- backend/src/utils/sendOtpEmail.js
+++ backend/src/utils/sendOtpEmail.js
@@ -5,8 +5,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendOtpEmail = async (toEmail, otp) => {
   try {
     const { data, error } = await resend.emails.send({
-      from: "QuickBook <onboarding@resend.dev>",
-      to: ["kb2426111@gmail.com"],
+      from: "QuickBook <onboarding@quick-book.in>",
+      to: [toEmail],
       subject: "QuickBook Email Verification OTP",
 
       html: `
